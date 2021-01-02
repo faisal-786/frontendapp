3fb7c75367ec17c7dde4cf09641a8b694d81de8a
faisal-786 <raofaisal123987@gmail.com>
2021-01-02 03:46:41 -0800

    bn

--- public/javascripts/jquerystar.js
+++ public/javascripts/jquerystar.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
   
-  /* 1. Visualizing things on Hover - See next part for action on click */
   $('#stars li').on('mouseover', function(){
-    var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
+    var onStar = parseInt($(this).data('value'), 10); 
    
-    // Now highlight all the stars that's not after the current hovered star
     $(this).parent().children('li.star').each(function(e){
       if (e < onStar) {
         $(this).addClass('hover');
@@ -21,9 +19,8 @@ $(document).ready(function(){
   });
   
   
-  /* 2. Action to perform on click */
   $('#stars li').on('click', function(){
-    var onStar = parseInt($(this).data('value'), 10); // The star currently selected
+    var onStar = parseInt($(this).data('value'), 10); 
     var stars = $(this).parent().children('li.star');
     
     for (i = 0; i < stars.length; i++) {
@@ -34,7 +31,6 @@ $(document).ready(function(){
       $(stars[i]).addClass('selected');
     }
     
-    // JUST RESPONSE (Not needed)
     var ratingValue = parseInt($('#stars li.selected').last().data('value'), 10);
     var msg = "";
     if (ratingValue > 1) {
